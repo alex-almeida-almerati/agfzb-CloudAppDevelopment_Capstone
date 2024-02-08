83d563d4039bfeb842c7f4234ef83cfe3be91917
Alex Almeida <alex.almeida@almerati.com>
2024-02-07 21:46:34 -0500

    intermedio Build CarModel and CarMake Django Models

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -1,12 +1,18 @@
 from django.contrib import admin
 # from .models import related models
-
+from .models import CarMake, CarModel
 
 # Register your models here.
+admin.site.register(CarMake)
+admin.site.register(CarModel)
 
 # CarModelInline class
+class CarModelInline(admin.StackedInline):
+    model = CarModel
 
 # CarModelAdmin class
+class CarModelAdmin(admin.ModelAdmin):
+    inlines = [CarModelInline]
 
 # CarMakeAdmin class with CarModelInline
 
--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -9,6 +9,14 @@ from django.utils.timezone import now
 # - Description
 # - Any other fields you would like to include in car make model
 # - __str__ method to print a car make object
+class CarMake(models.Model):
+    name = models.CharField(null=False, max_length=100)
+    description = models.CharField(null=False, max_length=100)
+    
+    def __str__(self):
+        message = ("Name: " + self.name + "\n" \
+                    + "Description: " + self.description)
+        return message
 
 
 # <HINT> Create a Car Model model `class CarModel(models.Model):`:
@@ -19,6 +27,30 @@ from django.utils.timezone import now
 # - Year (DateField)
 # - Any other fields you would like to include in car model
 # - __str__ method to print a car make object
+class CarModel(models.Model):
+    SEDAN = "Sedan"
+    SUV = "SUV"
+    WAGON = "Wagon"
+    COUPE = "Coupe"
+    TYPE_CHOICES = [
+        (SEDAN, "Sedan"),
+        (SUV, "SUV"),
+        (WAGON, "Wagon"),
+        (COUPE, "Coupe")
+    ]
+
+    carMake = models.ForeignKey(CarMake, null=False, on_delete=models.CASCADE)
+    name = models.CharField(null=False, max_length=100)
+    dealer_id = models.IntegerField()
+    type = models.CharField(null=False, max_length=5, choices=TYPE_CHOICES, default=SEDAN)
+    year = models.DateField(null=False)
+
+    def __str__(self):
+        message = ("Name: " + self.name + "\n"
+                    + "Dealer Id: " + self.dealer_id + "\n"
+                    + "Type: " + self.type + "\n"
+                    + "Year: " + self.year)
+        return message
 
 
 # <HINT> Create a plain Python class `CarDealer` to hold dealer data
